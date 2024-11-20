0638e21e11de7dcfbb6de36a54d6a01ce3e54ae2
Ryan Yogan <ryanyogan@gmail.com>
2024-11-20 10:54:00 -0600

    set page title in ChatRoomLive

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -25,7 +25,7 @@ defmodule SlaxWeb.ChatRoomLive do
           Chat.get_first_room!()
       end
 
-    {:noreply, assign(socket, hide_topic?: false, room: room)}
+    {:noreply, assign(socket, hide_topic?: false, room: room, page_title: "#" <> room.name)}
   end
 
   @impl true
