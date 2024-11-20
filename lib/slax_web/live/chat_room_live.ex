b97583e6f47843194e8ce85ccf21704594e8c891
Ryan Yogan <ryanyogan@gmail.com>
2024-11-20 13:59:41 -0600

    mix phx.gen.auth

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -6,7 +6,6 @@ defmodule SlaxWeb.ChatRoomLive do
   alias Slax.Chat
 
   @impl true
-  @spec mount(any(), any(), any()) :: {:ok, any()}
   def mount(_params, _session, socket) do
     rooms = Chat.list_rooms()
 
@@ -14,7 +13,6 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   @impl true
-  @spec handle_params(map(), any(), any()) :: {:noreply, any()}
   def handle_params(params, _uri, socket) do
     room =
       case Map.fetch(params, "id") do
@@ -29,7 +27,6 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   @impl true
-  @spec handle_event(<<_::96>>, any(), map()) :: {:noreply, map()}
   def handle_event("toggle-topic", _unsigned_params, socket) do
     {:noreply, update(socket, :hide_topic?, &(!&1))}
   end
