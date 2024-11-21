edb325e8f5c8074f3b75fb20760d787122a76bb4
Ryan Yogan <ryanyogan@gmail.com>
2024-11-20 21:34:21 -0600

    only show  delete button on hover

--- lib/slax_web/live/components/room_components.ex
+++ lib/slax_web/live/components/room_components.ex
@@ -31,13 +31,13 @@ defmodule SlaxWeb.Components.RoomComponents do
 
   def message(assigns) do
     ~H"""
-    <div id={@dom_id} class="relative flex px-4 py-3">
+    <div id={@dom_id} class="group relative flex px-4 py-3">
       <button
         :if={@current_user.id == @message.user_id}
         data-confirm="Are you sure?"
         phx-click="delete-message"
         phx-value-id={@message.id}
-        class="absolute top-4 right-4 text-red-500 hover:text-red-800 cursor-pointer"
+        class="absolute top-4 right-4 group-hover:block hidden text-red-500 hover:text-red-800 cursor-pointer"
       >
         <.icon name="hero-trash" class="size-4" />
       </button>
